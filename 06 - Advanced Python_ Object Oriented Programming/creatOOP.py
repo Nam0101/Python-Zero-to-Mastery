4035793e2bfbf8191dcae08dab7437bdd67da1d2
nvnkhy123 <Nam.NV205106@sis.hust.edu.vn>
2022-01-07 11:43:15 +0700

    update

--- 06 - Advanced Python_ Object Oriented Programming/creatOOP.py
+++ 06 - Advanced Python_ Object Oriented Programming/creatOOP.py
@@ -14,9 +14,20 @@ class player:
     def run(self, hello):
         print(f'{hello} my name is {self.name}')
 
+    @classmethod
+    def add_things(cls, num1, num2):
+        return cls('Teddy', num1+num2)
 
-player1 = player("nam", 20)
-player2 = player("Nhan", 21)
-print(player1.name, player1.age)
-player1.shout()
-player1.run("hello")
+    @staticmethod
+    def add_things(num1, num2):
+        return num1+num2
+
+
+#player1 = player("nam", 20)
+#player2 = player("Nhan", 21)
+#print(player1.name, player1.age)
+# printplayer1.shout()
+# player1.run("hello")
+player3 = player.add_things(2, 3)
+
+print(player3)
